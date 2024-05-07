3e989e1fa7362205cce905424f059b48a1073a53
shwetangi03 <113777115+shwetangi03@users.noreply.github.com>
2024-05-07 19:29:08 +0530

    Login Screen

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import Signup from './components/Signup'
+import SignupPage from './components/pages/SignupPage'
+import Navbar from './components/navbar/Navbar'
+import { Switch,Route,Redirect } from 'react-router-dom'
+import WelcomePage from './components/pages/WelcomePage'
+
 
 const App = () => {
   return (
     <div>
-      <Signup />
+      <Navbar />
+      <Switch>
+        <SignupPage path="/auth" />
+
+        <Route path="/welcome">
+          <WelcomePage />
+        </Route>
+
+        <Route path="*">
+          <Redirect to="/auth"/>
+        </Route>
+
+      </Switch>
     </div>
   )
 }
---
+++ src/components/pages/WelcomePage.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const WelcomePage = () => {
+  return (
+    <div>
+      <p className="text-3xl text-slate-600 p-4">
+        Welcome to expense tracker!!!
+      </p>
+      <hr className="border-gray-300 border-1"></hr>
+    </div>
+  );
+};
+
+export default WelcomePage;
