3203f72e1f26ec5340c2f09b733a2ab12adbb1a8
shwetangi03 <113777115+shwetangi03@users.noreply.github.com>
2024-05-10 10:58:52 +0530

    Logout user

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,14 +1,28 @@
-import React from 'react'
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 const Navbar = () => {
+const history = useHistory()
+
+  const logoutHandler = (event) => {
+    event.preventDefault();
+    localStorage.setItem("JWTTOKEN", "");
+    localStorage.setItem("userID", "");
+    localStorage.setItem("Email", "");
+    history.replace("./auth")
+  };
+
   return (
-    <div className='flex'>
-      <div>Home</div>
-      <div>Product</div>
-      <div>About Us</div>
+    <div className="flex p-2 gap-4">
+      <div className="px-2 p-1 bg-gray-400 text-white rounded-lg">Home</div>
+      <div className="px-2 p-1 bg-gray-400 text-white rounded-lg">Product</div>
+      <div className="px-2 p-1 bg-gray-400 text-white rounded-lg">About Us</div>
+      <div className="px-2 p-1 bg-gray-400 text-white rounded-lg">
+        <button onClick={logoutHandler}>Logout</button>
+      </div>
       <hr className="border-gray-300 border-1"></hr>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
